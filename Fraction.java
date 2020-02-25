407a7539708ff3508d75b7eebd9f88ca974914f1
Susinmn <susinmn@mail.ru>
2020-02-25 22:09:23 +0400

    Исправлены замечания

--- Fraction.java
+++ Fraction.java
@@ -1,3 +1,5 @@
+package net.susinmn;
+
 import java.lang.*;
 import java.math.BigDecimal;
 
@@ -9,52 +11,54 @@ jar cfv Fraction.jar Fraction.class
 */
 
 public class Fraction {
-    //TODO: реализовать атрибут intPart, plusMinus
+    private static final String FRACTION_DELIMITER = "/";
+    private static final int DECIMAL_MULTIPLIER = 10;
+    private static final long DEFAULT_DENOMINATOR = 1L;
+    public static final int MAXIMUM_ARGUMENTS_COUNT = 2;
+    public static final int MINIMUM_ARGUMENTS_COUNTS = 1;
+
     private long numerator;
     private long denominator;
 
     public static void main(String[] args) {
-        Fraction fraction1 = new Fraction(0.25);
+        Fraction fraction1 = Fraction.valueOf("5/10 1/2");
         System.out.println(fraction1);
     }
 
-    public Fraction(String s) {
-        s = s.trim();
-        String[] arr = s.split("/");
-        if (arr.length > 2) {
-            throw new IllegalArgumentException("Некорректная строка");
-        } else if (arr.length == 2) {
-            try {
-                this.numerator = Long.valueOf(arr[0]);
-                this.denominator = Long.valueOf(arr[1]);
-            } catch (Exception e) {
-                throw new IllegalArgumentException("Некорректная строка");
-            }
-        } else {
-            try {
-                this.numerator = Long.valueOf(arr[0]);
-                this.denominator = 1L;
-            } catch (Exception e) {
-                throw new IllegalArgumentException("Некорректная строка");
-            }
-
+    public Fraction(long numerator, long denominator) {
+        this.numerator = numerator;
+        this.denominator = denominator;
+        if (this.denominator == 0){
+            throw new IllegalArgumentException(Messages.ZERO_DENOMINATOR.getMessage());
         }
-        long factor = nod(this.numerator, this.denominator);
-        this.numerator /= factor;
-        this.denominator /= factor;
+        reduction();
     }
 
-    public Fraction(long intView) {
-        this.numerator = intView;
-        this.denominator = 1L;
+    private Fraction(long longView) {
+        this (longView, DEFAULT_DENOMINATOR);
     }
 
-    public Fraction(double doubleView){
-        this.numerator = (long) (doubleView * Math.pow(10, BigDecimal.valueOf(doubleView).scale()));
-        this.denominator = (long) Math.pow(10, BigDecimal.valueOf(doubleView).scale());
-        long factor = nod(this.numerator, this.denominator);
-        this.numerator /= factor;
-        this.denominator /= factor;
+    private Fraction(double doubleView){
+        this ((long) (doubleView * Math.pow(10, BigDecimal.valueOf(doubleView).scale())), (long) Math.pow(10, BigDecimal.valueOf(doubleView).scale()));
+    }
+
+    public static Fraction valueOf(String s) {
+        try {
+            s = s.trim();
+            String[] arr = s.split(FRACTION_DELIMITER);
+            if (arr.length == MAXIMUM_ARGUMENTS_COUNT) {
+                return new Fraction(Long.valueOf(arr[0]), Long.valueOf(arr[1]));
+            } else if (arr.length == MINIMUM_ARGUMENTS_COUNTS) {
+                return new Fraction(Long.valueOf(arr[0]));
+            } else {
+                throw new IllegalArgumentException(Messages.ILLEGAL_ARGUMENTS_COUNT.getMessage());
+            }
+        } catch (NumberFormatException ex){
+            throw new IllegalArgumentException(Messages.INCORRECT_STRING + " " + ex.getMessage());
+        } catch (Exception ex){
+            ex.printStackTrace();
+            throw new IllegalArgumentException(Messages.COMMON_CREATE_ERROR + " " + ex.getMessage());
+        }
     }
 
     public String toString() {
@@ -73,33 +77,25 @@ public class Fraction {
         //TODO: здесь нужно предусмотреть переполение long
         this.numerator = this.numerator * fract.denominator + fract.numerator * this.denominator;
         this.denominator = this.denominator * fract.denominator;
-        long factor = nod(this.numerator, this.denominator);
-        this.numerator /= factor;
-        this.denominator /= factor;
+        this.reduction();
     }
 
     public void subtraction(Fraction fract) {
         this.numerator = this.numerator * fract.denominator - fract.numerator * this.denominator;
         this.denominator = this.denominator * fract.denominator;
-        long factor = nod(this.numerator, this.denominator);
-        this.numerator /= factor;
-        this.denominator /= factor;
+        this.reduction();
     }
 
     public void multiplication(Fraction fract) {
         this.numerator = this.numerator * fract.numerator;
         this.denominator = this.denominator * fract.denominator;
-        long factor = nod(this.numerator, this.denominator);
-        this.numerator /= factor;
-        this.denominator /= factor;
+        this.reduction();
     }
 
     public void division(Fraction fract) {
         this.numerator = this.numerator * fract.denominator;
         this.denominator = this.denominator * fract.numerator;
-        long factor = nod(this.numerator, this.denominator);
-        this.numerator /= factor;
-        this.denominator /= factor;
+        this.reduction();
     }
 
     public long nod(long num1, long num2) {
@@ -113,4 +109,26 @@ public class Fraction {
 
     }
 
+    private void reduction() {
+        long factor = nod(numerator, denominator);
+        numerator = numerator / factor;
+        denominator = denominator / factor;
+    }
+
+    enum Messages {
+        ILLEGAL_ARGUMENTS_COUNT("Некорректное кол-во аргументов"),
+        ZERO_DENOMINATOR("Знаменатель не может быть нулем"),
+        INCORRECT_STRING("Некорректная строка"),
+        COMMON_CREATE_ERROR("Ошибка создания");
+
+        private String message;
+
+        Messages(String message) {
+            this.message = message;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+    }
 }
